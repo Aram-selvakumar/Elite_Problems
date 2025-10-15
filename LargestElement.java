7e664560d0f94ef902101ef4715b637c04ddca97
Aram-selvakumar <aramsk8888@gmail.com>
2025-10-15 09:04:48 +0530

    second commit

--- LargestElement.java
+++ LargestElement.java
@@ -1,6 +1,15 @@
+
 public class LargestElement {
     public static void main(String[]arg){
-        int number = 2,3,4,5;
+        int[] numbers = {10, 25, 7, 98, 56};
+        int largest = numbers[0]; 
 
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > largest) {
+                largest = numbers[i];
+            }
+        }
+
+        System.out.println("Largest element = " + largest);
 }
 }
--- Secondsmallest.java
+++ Secondsmallest.java
@@ -1,7 +1,11 @@
+import java.util.*;
 public class Secondsmallest {
     public static void main(String[] args) {
    int[] numbers={12,3,4,5};
-   
+   Arrays.sort(numbers);
+   System.out.println("Arrays.toString(numbers)");
+   int smalests=numbers[1];
+    System.out.println("Secondsmallest = "+smalests);
     }
 }
 
